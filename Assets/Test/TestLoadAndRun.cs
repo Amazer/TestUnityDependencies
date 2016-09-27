c42c7d4dd303a19dcd6fb7f3924e5e7b0efb2e18
Amazer <149779511@qq.com>
2016-09-27 19:59:57 +0800

    add test first load Initial,then load ui atlas

---
+++ Assets/Test/TestLoadAndRun.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Pal
+{
+	public class TestLoadAndRun : MonoBehaviour{
+
+		public  void Start () {
+            TestResourceLoader.LoadAssets(this, @"E:\CYCProject\TestDep\TestUnityDependencies\CYCData\Initial.assetbundle", "Initial",delegate(UnityEngine.Object obj)
+            {
+                if(obj==null)
+                {
+                    Debug.LogError("initial obj==null");
+                    return;
+                }
+                GameObject go = GameObject.Instantiate(obj) as GameObject;
+                go.name = "Initial";
+
+                TestResourceLoader.LoadAssets(this, @"E:\CYCProject\TestDep\TestUnityDependencies\CYCData\ShadersList.assetbundle", "ShaderList", delegate (UnityEngine.Object objShader)
+                 {
+                     if (objShader == null)
+                     {
+                         Debug.LogError("objShader ==null");
+                         return;
+                     }
+                     GameObject shaderGO = GameObject.Instantiate(objShader) as GameObject;
+                     shaderGO.name = "objShader";
+
+                     LoadUIAtlas();
+                 });
+            });
+		}
+        private void LoadUIAtlas()
+        {
+            StartCoroutine(LoadUIAtlas_corutine());
+        }
+        IEnumerator LoadUIAtlas_corutine()
+        {
+            int i = 0;
+            TestResourceLoader.LoadAssets(this, @"E:\CYCProject\TestDep\TestUnityDependencies\CYCData\AT_Fantasy.assetbundle", "Fantasy Atlas", delegate (UnityEngine.Object fobj)
+             {
+                 if (fobj == null)
+                 {
+                     Debug.LogError("fobj ==null");
+                     return;
+                 }
+                 GameObject fgo = GameObject.Instantiate(fobj) as GameObject;
+                 fgo.name = "fansy_atlas";
+                 ++i;
+             });
+            TestResourceLoader.LoadAssets(this, @"E:\CYCProject\TestDep\TestUnityDependencies\CYCData\AT_Wooden.assetbundle", "Wooden Atlas", delegate (UnityEngine.Object wobj)
+             {
+                 if (wobj == null)
+                 {
+                     Debug.LogError("wobj ==null");
+                     return;
+                 }
+                 GameObject wgo = GameObject.Instantiate(wobj) as GameObject;
+                 wgo.name = "wood_atlas";
+                 ++i;
+             });
+            while(i<2)
+            {
+                yield return null;
+            }
+            TestResourceLoader.LoadAssets(this, @"E:\CYCProject\TestDep\TestUnityDependencies\CYCData\UI_FlagPanel.assetbundle", "FlagPanel", delegate (UnityEngine.Object panelObj)
+             {
+                 if (panelObj == null)
+                 {
+                     Debug.LogError("panelObj ==null");
+                     return;
+                 }
+                 GameObject panelGO = GameObject.Instantiate(panelObj) as GameObject;
+                 panelGO.transform.parent = GameObject.Find("UI Root/Camera").transform;
+                 panelGO.transform.localScale = Vector3.one;
+
+             });
+
+        }
+
+	}
+}
---
+++ Assets/Test/TestResourceLoader.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+namespace Pal
+{
+	public class TestResourceLoader{
+        public static void LoadAssets(MonoBehaviour mb, string fullPath,string assetName,Action<UnityEngine.Object> cb)
+        {
+            new TestLoader().LoadAssets(mb,fullPath, assetName, cb);
+
+        }
+	}
+    public class TestLoader
+    {
+        public void LoadAssets(MonoBehaviour mb,string fullPath,string assetName,Action<UnityEngine.Object> cb)
+        {
+            
+            mb.StartCoroutine(this.ReadAssetbundle(fullPath, assetName, cb));
+
+        }
+         IEnumerator  ReadAssetbundle(string fullPath,string resName,Action<UnityEngine.Object> cb)
+        {
+
+            Debug.Log("fullPath:"+fullPath);
+            WWW w = new WWW("file:///" + fullPath);
+            while (!w.isDone)
+            {
+                if(w.error!=null)
+                {
+                    break;
+                }
+                yield return null;
+            }
+            if(w.error!=null)
+            {
+                Debug.LogError("load error:"+w.error);
+            }
+            else
+            {
+                if(!w.isDone)
+                {
+                    Debug.LogError("load not done:"+w.isDone);
+                }
+                else
+                {
+                    AssetBundle ab = w.assetBundle;
+                    if(ab.mainAsset!=null)
+                    {
+                        Debug.Log(" mainAsset:" + ab.mainAsset.name+",type:"+ab.mainAsset.GetType().FullName);
+                    }
+                    UnityEngine.Object[] all = ab.LoadAll();
+                    if (all != null)
+                    {
+                        Debug.Log("all.length:" + all.Length);
+                        for (int i = 0, imax = all.Length; i < imax; ++i)
+                        {
+                            if(all[i]!=null)
+                            {
+                                if(all[i].GetType()==typeof(GameObject)&& all[i].name==resName)
+                                {
+                                    if(cb!=null)
+                                    {
+                                        cb(all[i]);
+                                        break;
+                                    }
+                                }
+                                Debug.Log(i + " :" + all[i].name+",type:"+all[i].GetType().FullName);
+                            }
+                        }
+                    }
+//                    ab.Unload(true);
+                }
+
+            }
+
+
+        }
+    }
+}
